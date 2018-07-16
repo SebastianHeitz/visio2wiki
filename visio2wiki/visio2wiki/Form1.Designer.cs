c50658f9dba332c83395b63a6b225be5eed6a306
Sebastian Heitz <sebastianheitz@web.de>
2018-07-16 23:04:00 +0200

    Grundgerüst

---
+++ visio2wiki/visio2wiki/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace visio2wiki
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.prozessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.beendenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.hilfeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.benutzeranleitungToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.label1 = new System.Windows.Forms.Label();
+			this.tbxProjectName = new System.Windows.Forms.TextBox();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.label2 = new System.Windows.Forms.Label();
+			this.tbxTargetFolder = new System.Windows.Forms.TextBox();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.Filter = "Visio-Projekte|*.vdx|Alle Dateien|*.*";
+			this.openFileDialog1.Title = "Visio-Projekt laden";
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.prozessToolStripMenuItem,
+            this.hilfeToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(467, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// prozessToolStripMenuItem
+			// 
+			this.prozessToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripSeparator1,
+            this.beendenToolStripMenuItem});
+			this.prozessToolStripMenuItem.Name = "prozessToolStripMenuItem";
+			this.prozessToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+			this.prozessToolStripMenuItem.Text = "&Datei";
+			// 
+			// toolStripMenuItem1
+			// 
+			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+			this.toolStripMenuItem1.Size = new System.Drawing.Size(211, 22);
+			this.toolStripMenuItem1.Text = "Visio-Projekt laden...";
+			this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+			// 
+			// beendenToolStripMenuItem
+			// 
+			this.beendenToolStripMenuItem.Name = "beendenToolStripMenuItem";
+			this.beendenToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+			this.beendenToolStripMenuItem.Text = "&Beenden";
+			this.beendenToolStripMenuItem.Click += new System.EventHandler(this.beendenToolStripMenuItem_Click);
+			// 
+			// hilfeToolStripMenuItem
+			// 
+			this.hilfeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.benutzeranleitungToolStripMenuItem});
+			this.hilfeToolStripMenuItem.Name = "hilfeToolStripMenuItem";
+			this.hilfeToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+			this.hilfeToolStripMenuItem.Text = "Hilfe";
+			// 
+			// benutzeranleitungToolStripMenuItem
+			// 
+			this.benutzeranleitungToolStripMenuItem.Name = "benutzeranleitungToolStripMenuItem";
+			this.benutzeranleitungToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+			this.benutzeranleitungToolStripMenuItem.Text = "Benutzeranleitung";
+			this.benutzeranleitungToolStripMenuItem.Click += new System.EventHandler(this.benutzeranleitungToolStripMenuItem_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(36, 68);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(68, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Visio Projekt:";
+			// 
+			// tbxProjectName
+			// 
+			this.tbxProjectName.Location = new System.Drawing.Point(39, 85);
+			this.tbxProjectName.Name = "tbxProjectName";
+			this.tbxProjectName.ReadOnly = true;
+			this.tbxProjectName.Size = new System.Drawing.Size(384, 20);
+			this.tbxProjectName.TabIndex = 2;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(39, 152);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(77, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Zielverzeichnis";
+			// 
+			// tbxTargetFolder
+			// 
+			this.tbxTargetFolder.Location = new System.Drawing.Point(39, 168);
+			this.tbxTargetFolder.Name = "tbxTargetFolder";
+			this.tbxTargetFolder.ReadOnly = true;
+			this.tbxTargetFolder.Size = new System.Drawing.Size(384, 20);
+			this.tbxTargetFolder.TabIndex = 4;
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(208, 6);
+			// 
+			// toolStripMenuItem2
+			// 
+			this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+			this.toolStripMenuItem2.Size = new System.Drawing.Size(211, 22);
+			this.toolStripMenuItem2.Text = "Zielverzeichnis festlegen...";
+			this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(467, 575);
+			this.Controls.Add(this.tbxTargetFolder);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.tbxProjectName);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "Visio2Wiki";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem prozessToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem beendenToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem hilfeToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem benutzeranleitungToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox tbxProjectName;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox tbxTargetFolder;
+	}
+}
+
---
+++ visio2wiki/visio2wiki/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace visio2wiki
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void beendenToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			if (MessageBox.Show("Sind Sie sicher, dass Sie die Anwendung beenden möchten?", "Anwendung beenden", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+			{
+				Close();
+			}
+		}
+
+		private void benutzeranleitungToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Dieses kleine Programm konvertiert Visio-Prozesse in \"klickbare\" SVG-Grafiken und lädt sie in ein gegebenes Dokuwiki hoch.\nTest\nTest\nTest");
+			MessageBox.Show(openFileDialog1.FileName);
+		}
+
+		private void toolStripMenuItem1_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				tbxProjectName.Text = openFileDialog1.FileName;
+			}
+		}
+
+		private void toolStripMenuItem2_Click(object sender, EventArgs e)
+		{
+			if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+			{
+				tbxTargetFolder.Text = folderBrowserDialog1.SelectedPath;
+			}
+		}
+	}
+}
